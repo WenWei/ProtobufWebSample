b9658cbf3bb8d0f8bccb65ce44f86f3081763903
Chen, Wen-Wei <wenwei.chen@gmail.com>
2019-06-10 15:17:56 +0800

    add echo apicontroller

--- ProtobufWeb/Controllers/EchoController.cs
+++ ProtobufWeb/Controllers/EchoController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using ProtobufWeb.ProtoGen;
 
 namespace ProtobufWeb.Controllers
 {
@@ -12,8 +9,10 @@ namespace ProtobufWeb.Controllers
     {
         // POST api/Echo
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult<EchoData> Post([FromBody] EchoData value)
         {
+            value.Text = $"ECHO: {value.Text}";
+            return value;
         }
     }
 }
